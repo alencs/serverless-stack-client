c18ce9b293d2a5e9361d5411d46cfaab3db63264
Alen Toric <alen@Alens-MacBook-Pro.local>
2020-02-20 22:12:52 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-app-alen"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://ximle7xkzb.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1:7fee68ae-71b8-470f-ad41-981d9d2c46d0",
+    APP_CLIENT_ID: "1u0igi8u0897ndmbvg4d42enuq",
+    IDENTITY_POOL_ID: "eu-central-1:7fee68ae-71b8-470f-ad41-981d9d2c46d0"
+  }
+};
